f81c03e7bff81fa93129530c05cc8f4108f6187f
MaxCherry <patrick.wagner@agile-squad.de>
2019-05-31 23:00:11 +0200

    fixed sequence initialization

--- src/main/resources/init-data.sql
+++ src/main/resources/init-data.sql
@@ -1,3 +1,5 @@
 insert into CONTACT_PERSON (ID, SALUTATION, FIRST_NAME, LAST_NAME, PHONE_NUMBER) values (1,'MR', 'Atze', 'Peng', '+495551224');
 insert into CONTACT_PERSON (ID, SALUTATION, FIRST_NAME, LAST_NAME, PHONE_NUMBER) values (2,'MRS', 'Inge', 'Knall', '+495551225');
 insert into CONTACT_PERSON (ID, SALUTATION, FIRST_NAME, LAST_NAME, PHONE_NUMBER) values (3,'MR', 'Peter', 'Pow', '+495553324');
+
+alter sequence hibernate_sequence restart with 5;
