ff0e7bd7c50684424f59e55c1bb83c6a7b93d75a
MaxCherry <patrick.wagner@agile-squad.de>
2019-06-04 11:38:11 +0200

    added documenting junit spring boot test

---
+++ src/test/java/de/agile/springdemo/api/controller/DocumentingContactPersonControllerTest.java
@@ -0,0 +1,54 @@
+package de.agile.springdemo.api.controller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+public class DocumentingContactPersonControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation)).build();
+    }
+
+    @Test
+    public void listContactPersons() throws Exception {
+        this.mockMvc.perform(get("/api/contactpersons/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("list-contact-people", responseFields(
+                        subsectionWithPath("[]").description("Array of contact persons"),
+                        subsectionWithPath("[].id").description("Id of contact person"),
+                        subsectionWithPath("[].salutation").description("Salutation of contact person."),
+                        subsectionWithPath("[].firstName").description("First name of contact person"),
+                        subsectionWithPath("[].lastName").description("Last name of contact person")
+                )));
+    }
+}
